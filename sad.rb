65f929dc31cead229bcbe7cd6518e8c8a2f18157
Fly me to the moon <git@bigly.dog>
2020-05-25 14:27:17 -0700

    2020-05-25 14:27

---
+++ sad.rb
@@ -0,0 +1,13 @@
+class Sad < Formula
+  desc "Search and replace | Space Age seD"
+  homepage "https://github.com/ms-jpq/sad"
+  version "0.3.3"
+  bottle :unneeded
+
+  url "https://google.ca"
+  sha256 "a5a7259af1de4e971d4a2f58e0829e3b734f0ea827977a38bbcddbc138a0bbcb"
+
+  def install
+    bin.install "sad"
+  end
+end
